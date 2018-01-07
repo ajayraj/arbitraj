47a2f0ff0824a05e8597eece57dcc1789aa7ece5
Ajay Raj <ajay@ajayraj.co>
2018-01-07 08:19:52 -0800

    Finished incomplete edit, added Exmo

--- arbitrage.py
+++ arbitrage.py
@@ -4,24 +4,14 @@ import requests, json, itertools
 
 class Arbitrage:
 
-    def __init__(self):
-        self.rates = list()
-        
-        conversion = input("Enter currencies to exchange (ex. BTC-USD): ")
-        #Build list of exchange rates
-        rates.append(getCoinbase(conversion))
-        rates.append(getKraken(conversion))
-        rates.append(getBitfinex(conversion))
-
-        #Compute
-        bestRate()
+    rates = None
 
 
 
-    def getCoinbase(conversion):
+    def getCoinbase(self, conversion):
         url = "https://api.coinbase.com/v2/prices/"
         r = requests.get(url + conversion + "/buy")  #Coinbase BTC-USD
-        
+        print(conversion)
         if (r.status_code != requests.codes.ok):
             print("Requests error: ", r.status_code)
             exit()
@@ -44,7 +34,7 @@ class Arbitrage:
 
 
 
-    def getKraken(conversion):
+    def getKraken(self, conversion):
         
         url = "https://api.kraken.com/0/public/Ticker"
         
@@ -67,20 +57,32 @@ class Arbitrage:
         return (buy_price, sell_price, "Kraken")
 
     
-    def getBitfinex(conversion):
+    #def getBitfinex(conversion):
+
+    def getExmo(self, conversion):
+        url = "https://api.exmo.com/v1/order_book/?pair="
+
+        convformat = conversion.replace('-', '_')
+        r = requests.get(url + convformat)
+        r = r.json()
+
+        buy_price = r[convformat]["ask_top"]
+        sell_price = r[convformat]["bid_top"]
+
+        return(buy_price, sell_price, "Exmo")
 
 
 
+    def bestRate(self):
+       
+        lowestBuy = 0
+        highestSell = 0
 
-    def bestRate():
-
-        lowestBuy, highestSell = 0
-
-        for i in range(len(rates)):
+        for i in range(len(self.rates)):
             if (rates[i][0] < rates[lowestBuy][0]):
                     lowestBuy = i
 
-            if (rates[i][1] > rates[highestSell[1]):
+            if (rates[i][1] > rates[highestSell][1]):
                     highestSell = i
 
         gain = (float(rates[highestSell][1]) - float(rates[lowestBuy][0])) / float(rates[lowestBuy][0])
@@ -90,3 +92,22 @@ class Arbitrage:
 
         print(gain, "% gain | Buy: ", buyAt, " | Sell: ", sellAt)
 
+
+
+    def __init__(self):
+        rates = list()
+        
+        conversion = input("Enter currencies to exchange (ex. BTC-USD): ")
+        #Build list of exchange rates
+        rates.append(self.getCoinbase(conversion))
+        rates.append(self.getKraken(conversion))
+        rates.append(self.getExmo(conversion))
+        print(rates)
+        #rates.append(self.getBitfinex(conversion))
+
+        #Compute
+        #self.bestRate()
+
+
+
+Arbitrage()
