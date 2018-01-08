a2dbbc25047fa8e0e9943c780c522f7c75e1f063
Ajay Raj <ajay@ajayraj.co>
2018-01-08 12:24:04 -0800

    Formatting, added Bitfinex and potentials table

--- arbitrage.py
+++ arbitrage.py
@@ -57,19 +57,28 @@ class Arbitrage:
         return (buy_price, sell_price, "Kraken")
 
     
-    #def getBitfinex(conversion):
+    def getBitfinex(self, conversion):
+        url = "https://api.bitfinex.com/v1/ticker/"
+
+        convformat = conversion.replace('-', '')
+
+        r = requests.get(url + convformat).json()
+        
+        buy_price = r["ask"]
+        sell_price = r["bid"]
+
+        return (buy_price, sell_price, "Bitfinex")
 
     def getExmo(self, conversion):
         url = "https://api.exmo.com/v1/order_book/?pair="
 
         convformat = conversion.replace('-', '_')
-        r = requests.get(url + convformat)
-        r = r.json()
+        r = requests.get(url + convformat).json()
 
         buy_price = r[convformat]["ask_top"]
         sell_price = r[convformat]["bid_top"]
 
-        return(buy_price, sell_price, "Exmo")
+        return (buy_price, sell_price, "Exmo")
 
 
 
@@ -77,12 +86,18 @@ class Arbitrage:
        
         lowestBuy = 0
         highestSell = 0
+        gains = list()
 
 
-        print("Exchange    |    Lowest Ask (Buy)    |    Highest Bid (Sell)")
+        print("{0:^20s}|{1:^20s}|{2:^20s}".format("Exchange:", "Lowest Ask (Buy):", "Highest Bid (Sell):"))
 
         for i in range(len(rates)):
-            print(rates[i][2], "   |    ", rates[i][0], "   |    ", rates[i][1])
+            for j in range(len(rates)):
+                if i != j:
+                    gains_tup = (rates[i][2], rates[j][2], 100 * (float(rates[j][1]) - float(rates[i][0])) / float(rates[i][0]))
+                    gains.append(gains_tup)
+
+            print("{0:^20s}|{1:^20.6f}|{2:^20.6f}".format(rates[i][2], float(rates[i][0]), float(rates[i][1])))
             if (rates[i][0] < rates[lowestBuy][0]):
                     lowestBuy = i
 
@@ -90,6 +105,11 @@ class Arbitrage:
                     highestSell = i
 
         gain = 100 * (float(rates[highestSell][1]) - float(rates[lowestBuy][0])) / float(rates[lowestBuy][0])
+
+        print("\n{0:^20s}|{1:^20s}|{2:^20s}".format("Buy At:", "Sell At:", "% Gain:"))
+
+        for i in range(len(gains)):
+            print("{0:^20s}|{1:^20s}|{2:^20.4f}".format(gains[i][0], gains[i][1], gains[i][2]))
 
         buyAt = rates[lowestBuy][2]
         sellAt = rates[highestSell][2]
@@ -106,10 +126,11 @@ class Arbitrage:
         rates.append(self.getCoinbase(conversion))
         rates.append(self.getKraken(conversion))
         rates.append(self.getExmo(conversion))
-        print(rates)
-        #rates.append(self.getBitfinex(conversion))
+        rates.append(self.getBitfinex(conversion))
+        #self.getBitfinex(conversion)
 
         #Compute
+        print(rates)
         self.bestRate(rates)
 
 while(True):
